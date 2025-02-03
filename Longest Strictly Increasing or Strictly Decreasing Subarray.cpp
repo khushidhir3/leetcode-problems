355dbfa75af5c200c00dd388c3f32313c8201f99
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-02-03 19:07:39 +0530

    Create Longest Strictly Increasing or Strictly Decreasing Subarray.cpp

---
+++ Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
@@ -0,0 +1,25 @@
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int n=nums.size();
+        int inc = 1;
+        int dec = 1;
+        int maxlen=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]){
+                inc++;
+                dec=1;
+            }
+            else if(nums[i]<nums[i-1]){
+                dec++;
+                inc=1;
+            }
+            else{
+                inc=1;
+                dec=1;
+            }
+            int a=max(inc,dec);
+            maxlen=max(maxlen,a);
+          
+        }
+        return maxlen;
+    }
+};
