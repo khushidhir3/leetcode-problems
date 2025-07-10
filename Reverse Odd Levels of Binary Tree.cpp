c90c5afce5e90f60558464f7314b1cdbcc240058
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-07-10 18:29:47 +0530

    Create Reverse Odd Levels of Binary Tree.cpp

---
+++ Reverse Odd Levels of Binary Tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  void reverseodd(TreeNode* left,TreeNode* right,int level) {
+        if(!left||!right)
+        return;
+        if(level%2==1)
+        swap(left->val,right->val);
+        reverseodd(left->left,right->right,level+1);
+        reverseodd(left->right,right->left,level+1);
+    }
+     TreeNode* reverseOddLevels(TreeNode* root) {
+      if(!root)return root;
+      reverseodd(root->left,root->right,1);
+      return root;
+     }
+};
