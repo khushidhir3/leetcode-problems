98ae99973173ebb39993633144324a399679c16f
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-09-09 17:09:59 +0530

    Create Replace All Digits with Characters.cpp

---
+++ Replace All Digits with Characters.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+        int n=s.size();
+        for(int i=1;i<n;i+=2){
+            s[i]=(s[i]-'0')+(s[i-1]);
+        }
+        return s;
+        
+    }
+};
