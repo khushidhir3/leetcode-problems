ffa8479a7e741bf2bc9c3602d8b20208e609f229
Khushi Dhir <153167756+khushidhir3@users.noreply.github.com>
2025-09-19 21:24:28 +0530

    Create Number of Strings That Appear as Substrings in Word.cpp

---
+++ Number of Strings That Appear as Substrings in Word.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count=0;
+        for(auto i:patterns){
+            if(word.find(i)!=string::npos){
+                count++;
+            }
+        }
+        return count;
+    }
+};
